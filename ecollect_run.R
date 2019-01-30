068bc1e396cb111f183242fe80f00c0ce4100418
cecconifederico <cecconi.federico@gmail.com>
2019-01-30 16:38:42 +0100

    update search - serve debug
    update

--- ecollect_run.R
+++ ecollect_run.R
@@ -1,11 +1,12 @@
 hash_results <- list()
 
 # hash_results (#a:[1,1],#b:[1,1],#a:[3,2])
+# [1,4] significa 1 occorrenza al tempo 4
 
 
-for (i in c(1:3)){
+for (tempo in c(1:1)){
   print("inizia streaming")
-  rt <- stream_tweets(geocode = "37.78,-122.40,10mi", timeout = 15)
+  rt <- stream_tweets(geocode = "37.78,-122.40,10mi", timeout = 5)
   h <- rt$hashtags
   for (j in c(1:length(h))){
     k <- h[[j]]
@@ -14,5 +15,5 @@ for (i in c(1:3)){
     }
   }
   print("Inizia attesa")
-  Sys.sleep(time=10) 
+  Sys.sleep(time=1) 
 }
